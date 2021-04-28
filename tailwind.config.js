eaf29bbd1f173dbfe5d335fc4e4b8ac5cfb2ff40
Cyrus Yip <cyruscmyip1@gmail.com>
2021-04-28 15:23:59 -0700

    site not in progress anymore

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   mode: "jit",
   purge: ["./index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Montserrat", ...defaultTheme.fontFamily.sans],
+      },
+      backgroundSize: {
+        "200auto": "200% auto"
+      }
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
